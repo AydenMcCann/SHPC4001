aed986007a331f42a6d375541a76aaa52faf006d
AydenMcCann <79630714+AydenMcCann@users.noreply.github.com>
2021-03-11 13:44:34 +0800

    Update 3.1.py
    commenting

--- wk2/3.1.py
+++ wk2/3.1.py
@@ -7,7 +7,7 @@ import matplotlib as mpl
 def function(x):
     return np.sin(x) + (np.cos(2 * x)) ** 3 - x
 
-def bisect_root(f, lower, upper, eps, max_iter=100):
+def bisect_root(f, lower, upper, eps, max_iter=100):      
     """Find zero of a function in an interval to a given precision using the bis
    ection method.
     Arguments:
@@ -76,28 +76,28 @@ def secant_method(f, lower, upper, eps, max_iter=100):
 steps = 1000
 xval = np.empty(steps)
 xval[0] = 0
-dt = 0.001
-a0 = 0
+dt = 0.001  # size of timesteps
+a0 = 0  # upper and lower bounds
 b0 = 1
-newtonarray = np.empty(steps)
+newtonarray = np.empty(steps)       # empty arrays for results
 convergence = np.empty(steps)
 secantarray = np.empty(steps)
 bisectarray = np.empty(steps)
 
 for i in range(steps):
     xval[i] = 0.001 + i * dt
-    secantarray[i] = (secant_method(function, 0, 1, xval[i], 500))
+    secantarray[i] = (secant_method(function, 0, 1, xval[i], 500))  # performing calculations
     bisectarray[i] = (bisect_root(function, 0, 1, xval[i], 500))
     (temp, rr) = (optimize.newton(function, 1, full_output=True, rtol=xval[i]))
     newtonarray[i] = rr.iterations
-    convergence[i] = (np.log(b0 - a0) - np.log(xval[i])) / (np.log(2))
+    convergence[i] = (np.log(b0 - a0) - np.log(xval[i])) / (np.log(2))  # calculating analytical convergence of bisection method
 
 mpl.rcParams['font.family'] = 'Serif'
 plt.rcParams['font.size'] = 18
 plt.rcParams['axes.linewidth'] = 2
 plt.scatter(xval, newtonarray, s=2, label="Newton-Raphson Method Numerical Convergence")
-#plt.scatter(xval, secantarray, s=2, label="Secant Method Numerical Convergence")
-#plt.scatter(xval, bisectarray, s=2, label="Bisect Method Numerical Convergence")
+plt.scatter(xval, secantarray, s=2, label="Secant Method Numerical Convergence")            # plotting results
+plt.scatter(xval, bisectarray, s=2, label="Bisect Method Numerical Convergence")
 plt.scatter(xval, convergence, s=2, label="Bisect Method Analytical Convergence")
 plt.xlabel('t (target precision)')  # axis labels
 plt.ylabel('steps needed')
